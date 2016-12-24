ccf8b9535cdd77b3a0c7acac206dd6da07b4f991
Joe Thill <jtthill@iastate.edu>
2016-12-23 21:24:12 -0600

    Making bot parse messages

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 extern crate irc;
 
+use std::default::Default;
+use irc::client::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let cfg = Config {
+        nickname: Some(format!("rocketlobster")),
+        server: Some(format!("irc.chat.twitch.tv")),
+        port: Some(6667),
+        channels: Some(vec![format!("#rocketlobster")]),
+        password: Some(format!("oauth:607q5vftfd103fdft768l78m95qr8b")),
+        .. Default::default()
+    };
+
+    let server = IrcServer::from_config(cfg).unwrap();
+    server.identify().unwrap();
+    for message in server.iter() {
+        let message = message.unwrap(); //Just panic
+        println!("{}", message);
+    }
+    
 }
