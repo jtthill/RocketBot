53e2d1a8f800939a9e55670452d9072bfc24ba27
Thill <itcsv_jtthill@iastate.edu>
2016-12-27 16:14:35 -0600

    experimenting with threads

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate irc;
 
 use std::default::Default;
 use irc::client::prelude::*;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let cfg = Config {
@@ -15,15 +17,24 @@ fn main() {
 
     let server = IrcServer::from_config(cfg).unwrap();
     server.identify().unwrap();
-    for message in server.iter() {
-        let message = message.unwrap(); //Just panic
-        println!("{}", message);
-        match message.command {
-            Command::PRIVMSG(ref target, ref msg) => if msg.contains("pickles") {
-                server.send_privmsg(target, "Hi!").unwrap();
-            },
-            _ => (),
-        }
+    let server2 = server.clone();
+
+    thread::spawn(move || {
+        server2.iter().map(|m| print!("{}", m.unwrap())).count();
+    });
+    loop {
+        server.send_privmsg("#rocketlobster", "This is a message").unwrap();
+        thread::sleep(Duration::new(10, 0));
     }
+    // for message in server.iter() {
+    //     let message = message.unwrap(); //Just panic
+    //     println!("{}", message);
+    //     match message.command {
+    //         Command::PRIVMSG(ref target, ref msg) => if msg.contains("pickles") {
+    //             server.send_privmsg(target, "Hi!").unwrap();
+    //         },
+    //         _ => (),
+    //     }
+    // }
     
 }
